636862b2f1f7e43fbf05f963cda9bf5a1a2fe7d9
UdbhavU <UdbhavU@users.noreply.github.com>
2020-07-28 23:14:28 +0530

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const double bottomBarHeight = 75;
+const reusableCardColor = Color(0xff1d1e33);
+const bottomBarColor = Color(0xffd8004c);
+const inactiveColor = Color(0xff1d1e33);
+const activeColor = Color(0xffd8004c);
+const kNumberTextStyle =
+    TextStyle(color: Colors.white, fontSize: 80, fontWeight: FontWeight.bold);
+
+const labelTextStyle = TextStyle(color: Colors.white54, fontSize: 18);
